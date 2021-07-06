3f76007807f59d68a19d97e1a2cf35bc3754c166
maguilococo <56405247+maguilococo@users.noreply.github.com>
2021-07-06 12:36:22 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use('/api', routes);
+server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
