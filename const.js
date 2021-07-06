b26dff94f57dac6af52deebcf2dbc4d42fa0c09b
maguilococo <56405247+maguilococo@users.noreply.github.com>
2021-07-06 08:06:10 -0300

    deploy config

--- const.js
+++ const.js
@@ -1,10 +1,10 @@
 // Constants used in the backend
 
-const { apiKey } = process.env;
+const { DOGAPP_apiKey } = process.env;
 
 const BASE_URL = 'https://api.thedogapi.com/v1/breeds';
 const DOGS_URL = '/search?q=';
-const API_KEY = `apikey=${apiKey}`;
+const API_KEY = `apikey=${DOGAPP_apiKey}`;
 
 module.exports = {
     BASE_URL,
